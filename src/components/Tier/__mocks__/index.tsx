5c0b37ac2b62bcb1711b3759fb1fdb96bdd95608
Florian Eminger <florian.eminger@icloud.com>
2022-10-09 21:27:56 +0200

    .

--- src/components/Tier/__mocks__/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import tier from "../../../tierList/tier";
-export default function mock(props: { tier: tier }) {
-	return (
-		<div data-testid="TierListForObsidiaTier">
-			{JSON.stringify(props.tier.save())}
-		</div>
-	);
-}
